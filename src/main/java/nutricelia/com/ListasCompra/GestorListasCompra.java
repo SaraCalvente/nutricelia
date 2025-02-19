5fdc919db0a6ddf63616d2f031e57caa27487850
Raky76 <danigm03espacio@gmail.com>
2025-02-19 16:57:56 +0100

    Actualiz
    eliminarProducto
    buscarProducto

--- src/main/java/nutricelia/com/ListasCompra/GestorListasCompra.java
+++ src/main/java/nutricelia/com/ListasCompra/GestorListasCompra.java
@@ -6,7 +6,7 @@ import java.util.Vector;
 
 public class GestorListasCompra {
 
-    final private HashMap<String, ArrayList> mapa;
+    final private HashMap<String, ArrayList> mapa; //<NombreLista, Lista<Producto>>
 
     public GestorListasCompra() {
         this.mapa =  new HashMap<String, ArrayList>();
@@ -19,8 +19,23 @@ public class GestorListasCompra {
         mapa.get(nameLista).add(product);
     }
 
-    public void eliminatProducto(Object product) {
-        lista.remove(product);
+    synchronized public Object eliminarProducto(Object product, String nameLista, String nameProduct) {
+        Object productoEliminado = null;
+        if (mapa.containsKey(nameLista)){
+            productoEliminado = buscaProducto(mapa.get(nameLista),nameProduct);
+            if (productoEliminado != null){
+                mapa.get(nameLista).remove(productoEliminado);
+            }
+        }
+        return productoEliminado; // MODIFICAR
+    }
+    synchronized private Object buscaProducto(ArrayList lista, String nameProduct) { //implementado
+        for(Object product : lista ){
+            if (product == nameProduct){ //Aqui, product tendría que tener un atributo nombre al que hacerle un get para comparar
+                return product;
+            }
+        }
+        return null;
     }
 
 }
