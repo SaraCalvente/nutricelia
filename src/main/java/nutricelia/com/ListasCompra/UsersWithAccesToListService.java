66b2e373041ae3e3323a2e6f7ff93d99acd89d8f
Raky76 <danigm03espacio@gmail.com>
2025-02-24 18:25:02 +0100

    UsersWithAccesToListService Con el FIND by name bien
    Hay que corregir el FIND BY NAME de LISTED PRODUCT

---
+++ src/main/java/nutricelia/com/ListasCompra/UsersWithAccesToListService.java
@@ -0,0 +1,51 @@
+package nutricelia.com.ListasCompra;
+import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional;
+import io.smallrye.mutiny.Uni;
+import org.hibernate.ObjectNotFoundException;
+import jakarta.enterprise.context.ApplicationScoped;
+import java.util.List;
+
+@ApplicationScoped
+public class UsersWithAccesToListService {
+    public Uni<UsersWithAccesToList> findById(long id) {
+        return UsersWithAccesToList.<UsersWithAccesToList>findById(id)
+                .onItem().ifNull().failWith(() -> new
+                        ObjectNotFoundException(id, "UsersWithAccesToList"));
+    }
+
+    public Uni<UsersWithAccesToList> findByName(String email, int id_lista) {
+        return BuyList.find("email = ?1 and id_lista = ?2", email, id_lista).firstResult();
+    }
+
+    public Uni<List<BuyList>> list() {
+        return BuyList.listAll();
+    }
+
+    @ReactiveTransactional
+    public Uni<BuyList> create(BuyList buyList) {
+        return buyList.persistAndFlush();
+    }
+    /*
+    @ReactiveTransactional
+    public Uni<BuyList> update(BuyList buyList) {
+        return findById(buyList.id).chain(s -> s.merge(buyList));
+    }
+
+    @ReactiveTransactional
+    public Uni<Void> delete(long id) {
+        return findById(id)
+                .chain(u -> Uni.combine().all().unis(
+                                        Task.delete("BuyList.id", u.id),
+                                        Project.delete("BuyList.id", u.id)
+                                ).asTuple()
+                                .chain(t -> u.delete())
+                );
+
+    public Uni<BuyList> getCurrentBuyList() {
+        // TODO: replace implementation once security is added to the project
+        return BuyList.find("order by ID").firstResult();
+    }
+
+     */
+
+}
