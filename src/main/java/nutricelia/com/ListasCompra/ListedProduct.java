2dd01f1ff811f8192b346137f7aca253d6bd5bac
Raky76 <danigm03espacio@gmail.com>
2025-02-24 17:54:59 +0100

    ListedProductService creado
    Cambiado "ProductList" a "ListedProduct" para mejor legibilidad. Y creado ListedProductService

--- src/main/java/nutricelia/com/ListasCompra/ListedProduct.java
+++ src/main/java/nutricelia/com/ListasCompra/ListedProduct.java
@@ -1,15 +1,12 @@
 package nutricelia.com.ListasCompra;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.quarkus.hibernate.reactive.panache.PanacheEntity;
-import org.hibernate.annotations.CreationTimestamp;
 import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 @Entity
 @Table(name = "ProductosLista")
-public class ProductList extends PanacheEntity {
+public class ListedProduct extends PanacheEntity {
     @Column(nullable = false)
     int id_lista;
 
---
+++ src/main/java/nutricelia/com/ListasCompra/ListedProductService.java
@@ -0,0 +1,53 @@
+package nutricelia.com.ListasCompra;
+
+import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.hibernate.ObjectNotFoundException;
+
+import java.util.List;
+
+@ApplicationScoped
+public class ListedProductService {
+    public Uni<ListedProduct> findById(long id) {
+        return ListedProduct.<ListedProduct>findById(id)
+                .onItem().ifNull().failWith(() -> new
+                        ObjectNotFoundException(id, "ProductList"));
+    }
+
+    public Uni<BuyList> findByName(String name) {
+        return BuyList.find("nombre", name).firstResult();
+    }
+
+    public Uni<List<BuyList>> list() {
+        return BuyList.listAll();
+    }
+
+    @ReactiveTransactional
+    public Uni<BuyList> create(BuyList buyList) {
+        return buyList.persistAndFlush();
+    }
+    /*
+    @ReactiveTransactional
+    public Uni<BuyList> update(BuyList buyList) {
+        return findById(buyList.id).chain(s -> s.merge(buyList));
+    }
+
+    @ReactiveTransactional
+    public Uni<Void> delete(long id) {
+        return findById(id)
+                .chain(u -> Uni.combine().all().unis(
+                                        Task.delete("BuyList.id", u.id),
+                                        Project.delete("BuyList.id", u.id)
+                                ).asTuple()
+                                .chain(t -> u.delete())
+                );
+
+    public Uni<BuyList> getCurrentBuyList() {
+        // TODO: replace implementation once security is added to the project
+        return BuyList.find("order by ID").firstResult();
+    }
+
+     */
+
+}
