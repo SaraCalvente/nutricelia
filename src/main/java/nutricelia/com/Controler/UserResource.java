9fd6e51554362bffe4731016b61bc507303d0792
Jorge Villalonga Yuste <al415694@aula0132act.sg.uji.es>
2025-03-10 17:49:45 +0100

    Cerrado y borrado de cuenta

--- src/main/java/nutricelia/com/Controler/UserResource.java
+++ src/main/java/nutricelia/com/Controler/UserResource.java
@@ -1,9 +1,13 @@
 package nutricelia.com.Controler;
 
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
 import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
 import nutricelia.com.Model.User;
 import org.jboss.resteasy.reactive.ResponseStatus;
 
@@ -24,6 +28,16 @@ public class UserResource {
         return userService.list();
     }
 
+    @GET
+    @Path("/me")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<User> getUser(@Context SecurityContext securityContext) {
+        String email = securityContext.getUserPrincipal().getName(); // Obtiene el email del usuario autenticado
+        return userService.findByEmail(email)
+                .onItem().ifNull().failWith(() -> new WebApplicationException("Usuario no encontrado", 404));
+    }
+
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
@@ -34,14 +48,14 @@ public class UserResource {
 
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    @Path("{email: .+}") //Es posible que esto genere errores por el formato email
+    @Path("/modify/{email}") //Es posible que esto genere errores por el formato email
     public Uni<User> update(@PathParam("email") String name, User user) {
         user.nombre = name;
         return userService.update(user);
     }
 
     @DELETE
-    @Path("{email: .+}")
+    @Path("/delete/{email}")
     public Uni<Void> delete(@PathParam("email") String email) {
         return userService.delete(email);
     }
