26784e200eae6ae5a9d75fac19bd4dff8cebc3d1
GuilleRosSal <al415706@uji.es>
2025-03-17 16:08:40 +0100

    clase producto

---
+++ src/main/java/nutricelia/com/Model/Product.java
@@ -0,0 +1,26 @@
+package nutricelia.com.Model;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Producto")
+public class Product extends PanacheEntityBase {
+
+    @Id
+    @Column(unique = true, nullable = false)
+    public int id;
+    
+    @Column(nullable = false)
+    public String nombre;
+
+    @Column(nullable = false)
+    public int sinGluten;
+
+    @Column(nullable = false)
+    public String marca;
+
+}
